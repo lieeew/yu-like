445082c16a3081205c989e0589662060004bce86
pine <2268143474@qq.com>
2025-03-13 16:44:31 +0800

    第一期：1.1 项目初始化，引入日志和 knife4j

---
+++ src/main/java/com/yuyuan/thumb/ThumbBackendApplication.java
@@ -0,0 +1,16 @@
+package com.yuyuan.thumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pine
+ */
+@SpringBootApplication
+public class ThumbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThumbBackendApplication.class, args);
+    }
+
+}
